d50a44e1d36bdda06a5999e9476a2a6369c570bc
Lucas <pereirapro123@gmail.com>
2024-04-14 17:33:57 -0300

    Commit

--- loja-mks/__tests__/Produto.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import Produtos from "@/app/components/produtos/Produtos"
-import Home from "@/app/page"
-import { it } from "node:test"
-
-it("Renderizou o projeto", () => {
-    render(<Home/>)
-})
